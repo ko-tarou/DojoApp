8ed3a5d46669cdd3f5dabe0e04e2d6fb27b9953b
ko-tarou <tkota6174@gmail.com>
2025-01-23 13:46:35 +0900

    add CardListのCardにボタンを追加 ・挑戦ボタンを追加 ・css完成 ・ボタンを押されたときの処理だけ未実装

--- src/component/QuizCard/index.js
+++ src/component/QuizCard/index.js
@@ -3,12 +3,15 @@ import style from "../../styles/component/QuizCard/QuizCard.module.scss";
 export default function QuizCard({title, score}) {
     return (
         <div className={style.card}>
-            <div className={style.title}>
-                {title}
+            <div className={style.string}>
+                <div className={style.title}>
+                    {title}
+                </div>
+                <div className={style.score}>
+                    正答率：<span className={style.Bold}>{score}%</span>
+                </div>
             </div>
-            <div className={style.score}>
-                正答率：<span className={style.Bold}>{score}%</span>
-            </div>
+            <button className={style.customButton}>挑戦</button>
         </div>
     );
 }
