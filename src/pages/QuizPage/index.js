810aee05a6f2068f99937ec06f61b99280eed6fa
ko-tarou <tkota6174@gmail.com>
2025-01-25 12:38:26 +0900

    add 解答を入力する画面の作成 ・言語を選択するドロップダウンメニュー ・入力フォーム ・提出ボタンは未完成

--- src/pages/QuizPage/index.js
+++ src/pages/QuizPage/index.js
@@ -1,7 +1,16 @@
+import React,{ useState } from "react"
 import style from "../../styles/QuizPage/QuizPage.module.scss"
 import Header from "../Header"
 
 export default function QuizPage() {
+
+	const [selectedLanguage,setSelectedLanguage] = useState("Python");
+	const [code,setCode] = useState("");
+
+	const handleLanguageChange = (e) => {
+		setSelectedLanguage(e.target.value);
+	}
+
 	return (
 		<div>
 			<Header />
@@ -14,7 +23,7 @@ export default function QuizPage() {
 						問題文
 					</div>
 					<div className={style.container}>
-						正整数nの桁和を、nを1010進法で表したときの各桁の和と定義します。
+						正整数nの桁和を、nを10進法で表したときの各桁の和と定義します。
 						例えば20252025の桁和は2+0+2+5=92+0+2+5=9です。正整数nがnの桁和で割り切れる時、nを良い整数と呼びます。
 						例えば20252025はその桁和である99で割り切れるので良い整数です。
 						正整数の組(a,a+1)(a,a+1)であってaとa+1a+1が共に良い整数であるものを双子の良い整数と呼びます。
@@ -23,21 +32,35 @@ export default function QuizPage() {
 						そのような(a,a+1)(a,a+1)が存在しない場合はそのことを報告してください。
 					</div>
 				</div>
-				<div className={style.quiz}>
-					<div className={style.title}>
-						問題のタイトル
-					</div>
-					<div className={style.question}>
-						問題文
-					</div>
-					<div className={style.container}>
-						正整数nの桁和を、nを1010進法で表したときの各桁の和と定義します。
-						例えば20252025の桁和は2+0+2+5=92+0+2+5=9です。正整数nがnの桁和で割り切れる時、nを良い整数と呼びます。
-						例えば20252025はその桁和である99で割り切れるので良い整数です。
-						正整数の組(a,a+1)(a,a+1)であってaとa+1a+1が共に良い整数であるものを双子の良い整数と呼びます。
-						例えば(2024,2025)(2024,2025)は双子の良い整数です。
-						正整数Nが与えられます。N≤aN≤aかつa+1≤2Na+1≤2Nであるような双子の良い整数(a,a+1)(a,a+1)を発見してください。
-						そのような(a,a+1)(a,a+1)が存在しない場合はそのことを報告してください。
+				<div className={style.answer}>
+					<select
+						id="language-dropdown"
+						value={selectedLanguage}
+						onChange={handleLanguageChange}
+						className={style.language}
+					>
+						<option value="Python">Python</option>
+						<option value="Java">Java</option>
+						<option value="C">C</option>
+						<option value="C++">C++</option>
+						<option value="C#">C#</option>
+					</select>
+
+					<div className={style.codeeditorcontainer}>
+						<div className={style.linenumbers}>
+							{/* 行番号を動的に生成 */}
+								{code.split("\n").map((_, index) => (
+								<div key={index} className={style.numbers}>{index + 1}</div>
+								))}
+						</div>
+						<textarea
+							id="code-input"
+							className={style.codeinput}
+							value={code}
+							onChange={(e) => setCode(e.target.value)}
+							placeholder="Write your program here..."
+							rows="15"
+						></textarea>
 					</div>
 				</div>
 			</div>
